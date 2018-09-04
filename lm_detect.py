f65d50f053d35b93f17f0c1f24bb1cd1829a64b6
AyaLotfy <33145733+AyaLotfy@users.noreply.github.com>
2018-09-04 21:27:36 +0200

    Update lm_detect.py

--- lm_detect.py
+++ lm_detect.py
@@ -68,8 +68,8 @@ def img_compress(img, x_bins=100,y_bins=100):
 
 #train_dir = '../input/train'
 #test_dir = '../input/test'
-train_dir = '../train'
-test_dir = '../test'
+train_dir = '../Train'
+test_dir = '../Test'
 #Convert .jpg images into pixel arrays
 #imgs_train = [rgb_to_gray(mpimg.imread(train_dir + '/' + file, format = 'JPG')) for file in os.listdir(train_dir)]
 imgs_train_1 = [cv2.imread(train_dir + '/' + file) for file in os.listdir(train_dir)]
